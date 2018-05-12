ee9964b339c4ae0964b7c0916d370590ecd3a2ca
Jian Chen <jian.j.chen@gmail.com>
2018-05-12 13:57:54 +0100

    dir

--- Q.XLS/XlsUtils.cs
+++ Q.XLS/XlsUtils.cs
@@ -9,12 +9,19 @@ namespace Q.XLS
 {
   public static class XlsUtils
   {
-    [WorksheetFunction("QDir")]
-    public static XlOper[] Dir(string dn, string sel = "*")
+    [WorksheetFunction("QListFiles")]
+    public static XlOper[] ListFiles(string dn, string sel = "*")
     {
       DirectoryInfo d = new DirectoryInfo(dn);
       FileInfo[] Files = d.GetFiles(sel);
       var ret = Files.Select(x => x.Name.ToXlOper()).ToArray();
+      return ret;
+    }
+    [WorksheetFunction("QListDir")]
+    public static XlOper[] ListDir(string dir)
+    {
+      var folders = Directory.GetDirectories(dir);
+      var ret = folders.Select(x => x.ToXlOper()).ToArray();
       return ret;
     }
   }
